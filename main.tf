fce90ccf7c8662d5ed9c343171abbea7135de267
jianwei-chee <42338816+jianwei-chee@users.noreply.github.com>
2019-07-29 17:47:34 +0800

    committing after variable

--- main.tf
+++ main.tf
@@ -3,10 +3,35 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-01f7527546b557442"
+  ami           = "ami-0dad20bd1b9c8c004"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP
+  requests"
+type = number
+default = 8080
+}
+
